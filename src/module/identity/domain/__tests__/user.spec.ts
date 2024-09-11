98182e605edfa36ff1b6bea34fe0e65f5c30b8e7
Anderson Vieira <asvieira.br@gmail.com>
2024-09-11 11:10:28 -0300

    test: should instantiate a new user object successfully

---
+++ src/module/identity/domain/__tests__/user.spec.ts
@@ -0,0 +1,28 @@
+import { User } from '../entity/user';
+import { UserRole } from '../enum/user-role.enum';
+import { Identifier, FullName } from '../value-object';
+
+describe('User unit tests', () => {
+	it('should instantiate a new user object successfully', () => {
+		const userData = {
+			firstName: 'John',
+			lastName: 'Doe',
+			email: 'john@example.com',
+			role: UserRole.MENTEE,
+		};
+
+		const id = new Identifier();
+		const fullName = new FullName(userData.firstName, userData.lastName);
+
+		const newUser = new User(id, fullName, userData.email, userData.role);
+
+		expect(newUser.getId()).toBe(id);
+		expect(newUser.getFullName().getFirstName()).toBe(userData.firstName);
+		expect(newUser.getFullName().getLastName()).toBe(userData.lastName);
+		expect(newUser.getFullName().toString()).toBe(
+			`${userData.firstName} ${userData.lastName}`,
+		);
+		expect(newUser.getEmail()).toBe(userData.email);
+		expect(newUser.getRole()).toBe(userData.role);
+	});
+});
