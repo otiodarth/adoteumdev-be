ceabfa39df9b5b774bb0074c47911ffe68b0420a
Anderson Vieira <asvieira.br@gmail.com>
2024-09-13 09:29:34 -0300

    test: should UserManagementApplicationService returns a mapped object on create a new user

--- src/module/identity/application/service/user-management-application.service.spec.ts
+++ src/module/identity/application/service/user-management-application.service.spec.ts
@@ -2,9 +2,11 @@ import { FullName, Identifier, UserRole } from '@identity/domain/value-object';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { CreateUserInput } from '../input/create-user.input';
+import { CreateUserOutput } from '../output/create-user.output';
 import { EncryptService } from '@identity/domain/service/encrypt-service';
 import { IdentityDomainException } from '@identity/domain/exception/identity-domain.exception';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@identity/domain/entity/user';
 import { UserManagementApplicationService } from './user-management-application.service';
 import { UserManagementRepository } from '@persistence/typeorm/repository/user-management.repository';
 import { configDbMemory } from '@persistence/typeorm/config/typeorm-config-db-memory';
@@ -13,7 +15,7 @@ const validUserData: CreateUserInput = {
 	firstName: 'John',
 	lastName: 'Doe',
 	email: 'john@example.com',
-	role: new UserRole('mentee'),
+	role: 'mentee',
 	password: 'plainText',
 	passwordConfirmation: 'plainText',
 };
@@ -51,24 +53,28 @@ describe('UserManagementApplicationService', () => {
 	});
 
 	it('should create a new user and with correct values', async () => {
-		const createdUser = await service.createUser(validUserData);
+		const createdUser = new User(
+			new Identifier(),
+			new FullName(validUserData.firstName, validUserData.lastName),
+			validUserData.email,
+			new UserRole(validUserData.role),
+			'hashedPassword',
+		);
+
+		repository.create = jest.fn().mockResolvedValue(createdUser);
+
+		const result = await service.createUser(validUserData);
 
 		expect(encryptService.encrypt).toHaveBeenCalledWith(validUserData.password);
 		expect(repository.create).toHaveBeenCalledWith({
 			id: expect.any(Identifier),
 			fullName: new FullName(validUserData.firstName, validUserData.lastName),
 			email: validUserData.email,
-			role: validUserData.role,
+			role: new UserRole(validUserData.role),
 			password: 'hashedPassword',
 		});
 
-		expect(createdUser.getId().getId()).toBeTruthy();
-		expect(createdUser.getFullName()).toEqual(
-			new FullName(validUserData.firstName, validUserData.lastName),
-		);
-		expect(createdUser.getEmail()).toBe(validUserData.email);
-		expect(createdUser.getRole()).toEqual(validUserData.role);
-		expect(createdUser.getPassword()).toBe('hashedPassword');
+		expect(result).toEqual(CreateUserOutput.toOutput(createdUser));
 	});
 
 	it('should throw an exception if password and password confirmation do not match', async () => {
