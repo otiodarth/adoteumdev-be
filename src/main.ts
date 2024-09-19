e7f53d3850def5934b48d7c9324dbdf6920e7a7a
= <=>
2024-09-19 10:15:39 -0300

    chore: enable default cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api/v1');
+	app.enableCors();
 
 	app.useGlobalFilters(new GlobalExceptionFilter());
 	await app.listen(3000);
